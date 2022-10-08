04d1bacc23cd68b63bb1d1177c5ffa1189f2d2cd
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2022-10-08 18:56:09 +0530

    Create Count Substring k-1.cpp

---
+++ String/Count Substring k-1.cpp
@@ -0,0 +1,26 @@
+Count the substring equal to k-1
+  
+  int countOfSubstrings(string S, int K) {
+        // code 
+        int i = 0,j = 0,count = 0;
+        int n = S.length();
+        map<char , int>mp;
+        while(j<n){
+            mp[S[j]]++;
+            if(j-i+1 < K){
+                j++;
+            }
+            else if(j-i+1 == K){
+                if(mp.size() == K-1){
+                    count++;
+                }
+                mp[S[i]]--;
+                if(mp[S[i]]==0)
+                    mp.erase(S[i]);
+                    
+                i++;
+                j++;
+            }
+        }
+        return count;
+    }

960853f1fe62f607a3e6eca3851da5ae2a52f303
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2022-11-12 20:30:06 +0530

    Create Decrease a key.cpp

---
+++ Heap/Decrease a key.cpp
@@ -0,0 +1,10 @@
+To decrease a key
+
+void MinHeap::decreaseKey(int i, int new_val) 
+{
+    harr[i] = new_val;
+    while (i != 0 && harr[parent(i)] > harr[i]) {
+        swap(harr[i], harr[parent(i)]);
+        i = parent(i);
+    }
+}

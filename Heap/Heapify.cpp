dccebebb363f97f035f49d510376d5ded377a9bd
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2022-11-12 20:29:30 +0530

    Create Heapify.cpp

---
+++ Heap/Heapify.cpp
@@ -0,0 +1,14 @@
+Implementation of heapify
+
+void MinHeap::MinHeapify(int i) 
+{
+    int l = left(i);
+    int r = right(i);
+    int smallest = i;
+    if (l < heap_size && harr[l] < harr[i]) smallest = l;
+    if (r < heap_size && harr[r] < harr[smallest]) smallest = r;
+    if (smallest != i) {
+        swap(harr[i], harr[smallest]);
+        MinHeapify(smallest);
+    }
+}

07d8a7f6401d29cb39dedaa1cc50e0b9a58aee40
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2022-11-28 09:06:10 +0530

    Create Search element.cpp

---
+++ Tries/Search element.cpp
@@ -0,0 +1,17 @@
+Search a element from the tries
+
+bool search(struct TrieNode *root, string key) 
+{ 
+	struct TrieNode *pCrawl = root; 
+
+	for (int i = 0; i < key.length(); i++) 
+	{ 
+		int index = key[i] - 'a'; 
+		if (!pCrawl->children[index]) 
+			return false; 
+
+		pCrawl = pCrawl->children[index]; 
+	} 
+
+	return (pCrawl != NULL && pCrawl->isEndOfWord); 
+} 

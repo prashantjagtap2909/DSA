d496e6bf3f19b5dc3717ba36abacaafa5a0f3511
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2022-11-28 09:05:02 +0530

    Create insert a value.cpp

---
+++ Tries/insert a value.cpp
@@ -0,0 +1,18 @@
+Insert a value into a tries
+
+void insert(struct TrieNode *root, string key) 
+{ 
+	struct TrieNode *pCrawl = root; 
+
+	for (int i = 0; i < key.length(); i++) 
+	{ 
+		int index = key[i] - 'a'; 
+		if (!pCrawl->children[index]) 
+			pCrawl->children[index] = getNode(); 
+
+		pCrawl = pCrawl->children[index]; 
+	} 
+
+	 
+	pCrawl->isEndOfWord = true; 
+} 
